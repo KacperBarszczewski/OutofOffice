2c80f76f2cc785991e2078c89aa024d9105e0859
KacperB <kacpero.barszczewski@gmail.com>
2024-08-04 23:00:31 +0200

    add controller and dto for employee

---
+++ backend/API/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using API.Entities;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/employee")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly OutOfOfficeDbContext _dbContext;
+
+        public EmployeeController(OutOfOfficeDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<EmployeeDto>> GetAll()
+        {
+            var employees = _dbContext.Employees.ToList();
+
+            return Ok(employees);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<EmployeeDto> GetById([FromRoute] int id)
+        {
+            var employee = _dbContext.Employees.FirstOrDefault(e => e.ID == id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+    }
+}
---
+++ backend/API/Models/EmployeeDto.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities;
+
+namespace API.Models
+{
+    public class EmployeeDto
+    {
+        public int ID { get; set; }
+        public required string FullName { get; set; }
+        public required string Subdivision { get; set; }
+        public EmployeePosition Position { get; set; }
+        public EmployeeStatus Status { get; set; }
+        public int OutOfOfficeBalance { get; set; }
+        public string? Photo { get; set; }
+    }
+}
